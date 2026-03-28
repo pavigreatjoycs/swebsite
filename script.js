ccfe3bfacaa0441b9fd8009330a2dbc892fa6bf1
pavigreatjoycs <pavigreatjoycs@gmail.com>
2026-03-28 13:39:53 +0530

    adding website files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickBtn").addEventListener("click", function() {
+    const message = document.getElementById("message");
+    message.textContent = "Button clicked! 🎉";
+});
